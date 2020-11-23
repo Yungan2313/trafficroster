b433b1d7bd88f231b8a5f98625c0739364cabfe4
Yungan2313 <65326996+Yungan2313@users.noreply.github.com>
2020-11-23 17:02:41 +0800

    介面新增

--- editor.py
+++ editor.py
@@ -3,8 +3,26 @@ from tkinter import END
 from tkinter import Y
 from  tkinter import ttk
 from tkinter import Grid
+from functools import partial
 
+
+def lastedit():
+    a = 0
+def change_label_numer(dd):
+    if dd<10:
+        a = [btnm1,btntu1,btnw1,btnth1,btnf1,btnm2,btntu2,btnw2,btnth2,btnf2]
+        b = a[dd]
+        if c[dd] == 0:
+            b.configure(bg = "red",fg = "white")
+            c[dd] = 1
+        else:
+            b.configure(fg = "black",bg = 'gray93')
+            c[dd] = 0
+    else:
+        
 def choose():
+    m = [btnm1,btntu1,btnw1,btnth1,btnf1]
+    a = [btnm2,btntu2,btnw2,btnth2,btnf2]
     name = sum(listbox.curselection())
     selected_item = tree.get_children() ## get selected item
     tree.delete(selected_item)
@@ -17,11 +35,23 @@ def choose():
     daya = list(da.split(","))
     daymtxt = str(day[int(daym[0])])+str(day[int(daym[1])])+str(day[int(daym[2])])+str(day[int(daym[3])])+str(day[int(daym[4])])
     dayatxt = str(day[int(daya[0])])+str(day[int(daya[1])])+str(day[int(daya[2])])+str(day[int(daya[3])])+str(day[int(daya[4])])
-    if per[name][5] == 1:
+    if per[name][5] == "1":
         senbi = "是"
     else:
         senbi = "否"
     tree.insert("",0,text="資料" ,values=("{0}".format(per[name][0]),"{0}".format(daymtxt),"{0}".format(dayatxt),"{0}".format(per[name][3]),"{0}".format(senbi),"{0}".format(per[name][5]),"{0}".format(per[name][6]),"{0}".format(per[name][7]),"{0}".format(per[name][8])))
+    ennatxt.set(per[name][0])
+    for i in range(0,5):
+        if daym[i]!="0":
+            m[i].configure(bg = "red",fg = "white")
+        else:
+            m[i].configure(fg = "black",bg = 'gray93')
+    for i in range(0,5):
+        if daya[i]!="0":
+            a[i].configure(bg = "red",fg = "white")
+        else:
+            a[i].configure(fg = "black",bg = 'gray93')
+    entimetxt.set(per[name][3])
 def add():
     a = 0
     
@@ -29,6 +59,8 @@ def add():
 main = tk.Tk()
 main.geometry("1000x600")
 main.title("基礎設定")
+
+c = [0,0,0,0,0,0,0,0,0,0]
 
 ptxt = open("p.txt","r",encoding="utf-8")
 line = len(ptxt.readlines())
@@ -123,8 +155,77 @@ ch.pack(anchor = "sw")
 ad = tk.Button(main,text = "新增",command = add,font = ("新細明體",10),fg = "red")
 ad.pack(anchor = "sw")
 
+#label-name
+lana = tk.Label(main,text = "名字",font = ("新細明體",20))
+lana.pack(anchor = "sw")
+#label-morning
+lamo = tk.Label(main,text = "上午班次",font = ("新細明體",20))
+lamo.pack(anchor = "sw")
+#label-afternoon
+laaf = tk.Label(main,text = "下午班次",font = ("新細明體",20))
+laaf.pack(anchor = "sw")
+#label-day
+lada = tk.Label(main,text = "值勤幾次",font = ("新細明體",20))
+lada.pack(anchor = "sw")
+#label-senbi
+lase = tk.Label(main,text = "學長姐(不值大檔魯班)",font = ("新細明體",20))
+lase.pack(anchor = "sw")
+#label-right
+lari = tk.Label(main,text = "正打",font = ("新細明體",20))
+lari.pack(anchor = "sw")
+#label-right time
+lariti = tk.Label(main,text = "正打次數",font = ("新細明體",20))
+lariti.pack(anchor = "sw")
+#label-two
+latw = tk.Label(main,text = "二燈",font = ("新細明體",20))
+latw.pack(anchor = "sw")
+#label-big
+labi = tk.Label(main,text = "大燈",font = ("新細明體",20))
+labi.pack(anchor = "sw")
+#entry-name
+ennatxt = tk.StringVar()
+enna = tk.Entry(main,textvariable = ennatxt)
+enna.place(x=520,y=158)
+#button-morning
+txtmo = [0,0,0,0,0]
+day = ["一","二","三","四","五"]
+for i in range(0,5):
+    txtmo[i] = tk.StringVar()
+    txtmo[i].set(str(day[i]))
+btnm1 = tk.Button(main,textvariable = txtmo[0],command = partial(change_label_numer,0),font = ("新細明體",15))
+btntu1 = tk.Button(main,textvariable = txtmo[1],command = partial(change_label_numer,1),font = ("新細明體",15))
+btnw1 = tk.Button(main,textvariable = txtmo[2],command = partial(change_label_numer,2),font = ("新細明體",15))
+btnth1 = tk.Button(main,textvariable = txtmo[3],command = partial(change_label_numer,3),font = ("新細明體",15))
+btnf1 = tk.Button(main,textvariable = txtmo[4],command = partial(change_label_numer,4),font = ("新細明體",15))
+btnm1.place(x=520,y=180)
+btntu1.place(x=560,y=180)
+btnw1.place(x=600,y=180)
+btnth1.place(x=640,y=180)
+btnf1.place(x=680,y=180)
+#-----
+txtaf = [0,0,0,0,0]
+for i in range(0,5):
+    txtaf[i] = tk.StringVar()
+    txtaf[i].set(str(day[i]))
+btnm2 = tk.Button(main,textvariable = txtaf[0],command = partial(change_label_numer,5),font = ("新細明體",15))
+btntu2 = tk.Button(main,textvariable = txtaf[1],command = partial(change_label_numer,6),font = ("新細明體",15))
+btnw2 = tk.Button(main,textvariable = txtaf[2],command = partial(change_label_numer,7),font = ("新細明體",15))
+btnth2 = tk.Button(main,textvariable = txtaf[3],command = partial(change_label_numer,8),font = ("新細明體",15))
+btnf2 = tk.Button(main,textvariable = txtaf[4],command = partial(change_label_numer,9),font = ("新細明體",15))
+btnm2.place(x=520,y=210)
+btntu2.place(x=560,y=210)
+btnw2.place(x=600,y=210)
+btnth2.place(x=640,y=210)
+btnf2.place(x=680,y=210)
+#entry-times
+entimetxt = tk.StringVar()
+entime = tk.Entry(main,textvariable = entimetxt)
+entime.place(x=520,y=255)
+#button-senbi
+butsen = tk.Button(main,text = "是",command = no)
+
 #button-edit
-edit = tk.Button(main,text = "修改",command = add,font = ("新細明體",10),fg = "red")
+edit = tk.Button(main,text = "修改",command = lastedit ,font = ("新細明體",10),fg = "red")
 edit.pack(side = "bottom")
 
 main.mainloop()
